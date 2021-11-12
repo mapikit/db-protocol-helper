f85e739c2bc9f0e08b5fea7376faa1262d77999b
Fábio Meneses Junior <flgmjr@gmail.com>
2021-11-11 23:10:16 -0300

    configure repo and describe requirements in the index file

---
+++ index.ts
@@ -0,0 +1,16 @@
+// We should provide:
+// - Function to check schema diffs (should be based on their IDs)
+//     > Receives 2 lists of Schemas, and outputs the schema diffs between them;
+// - Function to force the user to check a schema update:
+//     > Receives in a SchemaDiff and synchronously prompts in the user to confirm updates;
+// - Abstract class providing the types and required interfaces
+// - Function to get the class and its descriptor file (db-protocol.json) from a given path;
+// - Function to validate the descriptor file;
+// - Function to validate if the exported class correctly implements the class and its described extensions;
+//
+// The descriptor file should have:
+// - Name
+// - Version
+// - Description
+// - Protocol entrypoint (file where the class is exported)
+// - MainClass (Exported class constructor)
